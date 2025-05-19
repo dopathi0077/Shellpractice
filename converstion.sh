3cb46779aece16b71f606550990fb9e104308b42
varundopathi <varun.reddy0077@gmail.com>
2025-05-19 10:49:37 +0530

    added hello world code

--- converstion.sh
+++ converstion.sh
@@ -3,3 +3,4 @@
 echo "Varun: How are you practicing Devops class"
 echo "Sai: Iam doing good.How are you doing?"
 echo "Varun: Iam bit back in practising but i will complete it soon."
+echo "Sai: ok very good!. All the best"
