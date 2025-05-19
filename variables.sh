cae8bd7e6500123a33fba7b538fecd5fcc047f73
varundopathi <varun.reddy0077@gmail.com>
2025-05-19 10:54:21 +0530

    added hello world code

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+PERSON1=Varun
+PERSON2=Sai
+
+echo "$PERSON1: Hey $PERSON2 How are you practicing Devops class"
+echo "$PERSON2: Iam doing good.How are you doing?"
+echo "$PERSON1: Iam bit back in practising but i will complete it soon."
+echo "$PERSON2: ok very good!. All the best"
