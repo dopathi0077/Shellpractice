6ec0dc9af1657397efe9450531f354da1a096082
varundopathi <varun.reddy0077@gmail.com>
2025-05-21 16:35:25 +0530

    added new file

--- install_commands.sh
+++ install_commands.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "Please run the script with root access"
+   exit 1 # give other than 0 till 127
 else
    echo "You are running with root access"
 fi
