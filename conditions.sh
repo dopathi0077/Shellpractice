9d0b0c6eddc8add25228352625719341556a06ce
varundopathi <varun.reddy0077@gmail.com>
2025-05-21 16:21:46 +0530

    added new file

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 Number=$1   
 
-if [$Number -lt 10]
+if [ $Number -lt 10 ]
 then
-echo "Given $Number is less than 10"
+   echo "Given $Number is less than 10"
 else
-echo "Given $Number is not less than 10"
+   echo "Given $Number is not less than 10"
 fi
