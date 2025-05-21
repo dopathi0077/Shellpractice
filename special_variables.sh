d06016d5fa12746e031c8da3a035bbdbce43c0c2
varundopathi <varun.reddy0077@gmail.com>
2025-05-21 16:04:49 +0530

    added new file

--- special_variables.sh
+++ special_variables.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to script: $@"
 echo "Number of varibles passed: $#"
+echo "Script name is : $0"
+echo "Current directory is: $PWD"
+echo " User running the script: $USER"
+echo "PID of script: $$"
+sleep 10 &
+echo "PID of background process: $?"
