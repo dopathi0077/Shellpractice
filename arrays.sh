2d32ca2573556782ea674a8724a0552adb2feb55
varundopathi <varun.reddy0077@gmail.com>
2025-05-21 15:43:03 +0530

    added new file

--- arrays.sh
+++ arrays.sh
@@ -5,6 +5,6 @@ Movies=("Single" "Court" "Sie")
 echo "First movie is : ${Movies[0]}"
 echo "Second movie is : ${Movies[1]}"
 
-echo "All  movies are  : ${Movies[@]}"
+echo "All movies are  : ${Movies[@],}"
 
 
