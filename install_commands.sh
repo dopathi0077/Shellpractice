ee3415d6c3c1c8b3a39b2d85b4c1352c826f1670
varundopathi <varun.reddy0077@gmail.com>
2025-05-21 16:39:02 +0530

    added new file

--- install_commands.sh
+++ install_commands.sh
@@ -4,10 +4,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo "Please run the script with root access"
+   echo "ERROR: Please run the script with root access"
    exit 1 # give other than 0 till 127
 else
    echo "You are running with root access"
 fi
 
 dnf install nginx -y
+
+if [ $? -eq 0 ]
+then 
+  echo "installation of nginx is ... SUCCESS"
+else
+  echo "installation of nginx is ... FAILURE"
+fi
