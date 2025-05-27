64666f7b5e2aa146928256f0c65adaf67581aaf8
varundopathi <varun.reddy0077@gmail.com>
2025-05-27 13:23:05 +0530

    added new file

--- colors.sh
+++ colors.sh
@@ -34,23 +34,23 @@ else
    echo -e "Nginx is already $Y installed no changes to do $N !"
 fi 
 
-# dnf list installed msql 
-# if [ $? -ne 0 ] 
-# then 
-#      echo "MySQL is not installed... going to install it"
-#      dnf install mysql -y
-#      VALIDATE $? "MySQL"
-# else
-#      echo -e "$Y msql is already  installed no changes to do ! $N "
-# fi 
-
-
-dnf list installed mysql
-if [ $? -ne 0 ]
-then
-    echo "MySQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
+dnf list installed msql 
+if [ $? -ne 0 ] 
+then 
+     echo "MySQL is not installed... going to install it"
+     dnf install mysql -y
+     VALIDATE $? "MySQL"
 else
-    echo -e "Nothing to do MySQL... $Y already installed $N"
-fi
+     echo -e "$Y msql is already  installed no changes to do ! $N "
+fi 
+
+
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then
+#     echo "MySQL is not installed... going to install it"
+#     dnf install mysql -y
+#     VALIDATE $? "MySQL"
+# else
+#     echo -e "Nothing to do MySQL... $Y already installed $N"
+# fi
