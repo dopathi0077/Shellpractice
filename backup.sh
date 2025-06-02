1fcc85dde3c4c86fb51bfeca3ba71ec21d39e91a
varundopathi <varun.reddy0077@gmail.com>
2025-06-02 13:15:39 +0530

    added new file

---
+++ backup.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-10} # if days are not provided in input then it will take 10 days default or it will take passed value
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/Shellpractice-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+#checkif user has root privilages or not 
+check_root(){
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR: $R Please run the script with root access $N " | tee -a $LOG_FILE
+   exit 1 # give other than 0 till 127
+else
+   echo "You are running with root access" | tee -a $LOG_FILE
+fi
+}
+check_root
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE: $N sh backup.sh <source-dir> <destination-dir> <days(optional)>"
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ] # it will pass the exit status args VALIDATE $? "nginx" as $1 and $2 to this 
+    then
+      echo -e "$2 is .... $G Succesfull $N" | tee -a $LOG_FILE
+    else
+      echo -e "$2 is .... $R Failure $N" | tee -a $LOG_FILE
+      exit 1
+    fi
+}
